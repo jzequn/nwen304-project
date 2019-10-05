95957beba00f952e8fd074b8cad50f1e39da3973
zequn <jzequn@outlook.com>
2019-10-05 15:50:46 +1300

    now only display one message, no more duplicated message will be displayed when login /logout/ register. Create a login link and register link under register and login page respectively

--- controllers/user.js
+++ controllers/user.js
@@ -3,40 +3,11 @@ const User = require('../models/user.model');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 
-// exports.postUser = (req, res) => {
-//     // Finds the validation errors in this request and wraps them in an object with handy functions
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         return res.status(422).json({ errors: errors.array() });
-//     }
-
-//     User.create({
-//         username: req.body.username,
-//         password: req.body.password
-//     }).then(user => res.json(user))
-// }
-
-
 exports.getRegister = (req, res, next) => {
     res.render('users/register');
 }
 
 exports.postRegister = (req, res, next) => {
-    // only match the form name instead of form id
-    // const username = req.body.Username;
-    // const email = req.body.InputEmail;
-    // const password = req.body.InputPassword;
-    // const repeatPassword = req.body.RepeatPassword;
-
-    // const user = new User(username, email, password);
-    // user
-    //     .save()
-    //     .then(() => {
-    //         res.redirect('/');
-    //     })
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
     const { Username, InputEmail, InputPassword, RepeatPassword } = req.body;
     const errors = [];
 
@@ -82,7 +53,7 @@ exports.postRegister = (req, res, next) => {
                 bcrypt.genSalt(10, (err, salt) => {
                     bcrypt.hash(InputPassword, salt, (err, hash) => {
                         if (err) throw err;
-                        console.log('hash:', hash, 'InputPassword:', InputPassword)
+                        // console.log('hash:', hash, 'InputPassword:', InputPassword)
                         // InputPassword = hash;
 
 
@@ -96,7 +67,7 @@ exports.postRegister = (req, res, next) => {
                                     'success_msg',
                                     'You are now registered and can log in'
                                 );
-                                console.log('user msg:', req.flash.success_msg)
+                                // console.log('user msg:', req.flash.success_msg)
                                 res.redirect('/users/login');
 
                                 // console.log('create user', result)
@@ -117,7 +88,7 @@ exports.getLogin = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-    console.log('users controller postLogin - post login')
+    // console.log('users controller postLogin - post login')
     passport.authenticate('local', {
         successRedirect: '/users/dashboard',
         failureRedirect: '/users/login',
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 # How to use
 
+```
 1. download the project
 2. run npm install
 3. create .env file in the root folder, save it with DATABASE_URL
 
 4. run 'npm start' or 'npm run dev' to start the app
+```
