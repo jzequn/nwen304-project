217d6b5b0a7301f5da992c67ad5f8267e4271e6f
Antony Helsby <antonyhelsby@gmail.com>
2019-10-19 12:57:24 +1300

    tiny changes

--- util/database.js
+++ util/database.js
@@ -4,15 +4,7 @@ const Sequelize = require('sequelize')
 // heroku postgresql connect to nodejs app 
 // don't forget to setup environment variabls with dotenv in the nodejs app 
 
-<<<<<<< Updated upstream
 sequelize = new Sequelize(process.env.DATABASE_URL, {
-=======
-// Antony currently pasting this in directly when using on his end otherwise there is an error...
-// postgres://fxnaccwfwmhrvr:c896d777af39a82546f0520986908945d1376a8bb506a7ec3f00b015ac16caaf@ec2-54-235-92-244.compute-1.amazonaws.com:5432/dej52ufjgas7g6
-// process.env.DATABASE_URL
-
-sequelize = new Sequelize("postgres://fxnaccwfwmhrvr:c896d777af39a82546f0520986908945d1376a8bb506a7ec3f00b015ac16caaf@ec2-54-235-92-244.compute-1.amazonaws.com:5432/dej52ufjgas7g6", {
->>>>>>> Stashed changes
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
