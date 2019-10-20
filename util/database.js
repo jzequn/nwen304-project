d1d5fecf904e939510a79a3be0d32b4f34c2fc39
Antony Helsby <antonyhelsby@gmail.com>
2019-10-21 00:43:53 +1300

    can click pictures to go to product

--- util/database.js
+++ util/database.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize')
 // heroku postgresql connect to nodejs app 
 // don't forget to setup environment variabls with dotenv in the nodejs app 
 
-sequelize = new Sequelize("postgres://fxnaccwfwmhrvr:c896d777af39a82546f0520986908945d1376a8bb506a7ec3f00b015ac16caaf@ec2-54-235-92-244.compute-1.amazonaws.com:5432/dej52ufjgas7g6", {
+
+sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
